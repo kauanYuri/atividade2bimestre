f2b9737bf922d0f46e0952aea97835b2daf4be4b
kauanYuri <kauanspfc16@gmail.com>
2019-07-02 11:24:34 -0300

    Primeiro commit

---
+++ src/main/java/com/ifpi/ted2019/ProcessoApplication.java
@@ -0,0 +1,13 @@
+package com.ifpi.ted2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoApplication.class, args);
+	}
+
+}
